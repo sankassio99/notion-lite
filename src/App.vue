7a29ca6ad17cf4186a08cf2ef9502850d4dd2e29
Kássio San <kciosantana@gmail.com>
2025-04-26 21:02:04 -0300

    style as tailwind css

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,31 @@
 <template>
-    <div id="sidebar" class="w-[250px] bg-gray-100 p-4 border-r border-gray-300 h-screen">
-        <button @click="createNewNote" class="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
-            + Nova Nota
-        </button>
-        <ul class="mt-4 space-y-2">
-            <li
-                v-for="note in notes"
-                :key="note"
-                @click="loadNote(note)"
-                class="cursor-pointer hover:bg-gray-200 p-2 rounded line-clamp-2"
-            >
-                {{ note }}
-            </li>
-        </ul>
-    </div>
-    <div id="editor" class="w-3/4 py-6 px-8">
-        <div class="controls h-3 p-1 flex items-end justify-end space-x-2">
-            <button @click="deleteCurrentNote">
-                <span class="text-gray-500 hover:text-red-500">Excluir</span>
+    <div class="flex">
+        <div id="sidebar" class="w-[250px] bg-gray-100 p-4 border-r border-gray-300 h-screen">
+            <button @click="createNewNote" class="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600">
+                + Nova Nota
             </button>
-            <button @click="saveNote">
-                <span class="text-gray-500 hover:text-blue-500">Salvar</span>
-            </button>
+            <ul class="mt-4 space-y-2">
+                <li v-for="note in notes" :key="note" @click="loadNote(note)"
+                    class="cursor-pointer hover:bg-gray-200 p-2 rounded line-clamp-2">
+                    {{ note }}
+                </li>
+            </ul>
         </div>
+        <div id="editor" class="w-3/4 py-6 px-8">
+            <div class="controls h-3 p-1 flex items-end justify-end space-x-2">
+                <button @click="deleteCurrentNote">
+                    <span class="text-gray-500 hover:text-red-500">Excluir</span>
+                </button>
+                <button @click="saveNote">
+                    <span class="text-gray-500 hover:text-blue-500">Salvar</span>
+                </button>
+            </div>
 
-        <input
-            v-model="noteTitle"
-            class="w-full text-2xl font-bold border-b border-gray-300 focus:outline-none focus:border-blue-500 mb-4"
-            placeholder="Título da nota"
-        />
-        <div id="editorjs" class="ml-2 rounded p-4"></div>
+            <input v-model="noteTitle"
+                class="w-full text-2xl font-bold border-b border-gray-300 focus:outline-none focus:border-blue-500 mb-4"
+                placeholder="Título da nota" />
+            <div id="editorjs" class="ml-2 rounded p-4"></div>
+        </div>
     </div>
 </template>
 
